dc73e04583aabff12e8ca9f46469fa9b8ea1b597
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2023-12-23 18:10:54 +0530

    JavaScript Class_1

---
+++ Learning/JavaScript/class_1.js
@@ -0,0 +1,51 @@
+// Types of console.log
+
+let userName
+const password = null
+
+console.log(userName)
+console.log(password)
+// console.error("")
+// console.warn("hardik")
+
+
+// let 
+// const
+
+// Primitive DataType (STACK)
+// we store primitive data type in stack kind of memory
+/*
+string
+Number
+Bool
+BigInt
+Symbol
+Null
+Undefined
+*/
+
+// Reference or Non-Primitive Datatype (HEAP)
+// we store non-primitive data type in heap kind of memory
+
+/*
+Array
+Object
+Functions
+Map
+Set
+Date
+RegExp 
+var a = "Hello World"
+console.log(typeof a)
+var b = new String("Hello World")
+console.log(b instanceof String)
+var num = 10;
+console.log(typeof num);
+*/
+
+// let a = []
+
+// Comparison Operator
+
+// == :- Checks the value 
+// === :- Checks the DataType
