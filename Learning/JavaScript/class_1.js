66f691b0efc22a8d8bcf6fe2ceaa2305240c0257
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2023-12-23 22:02:12 +0530

    Modify the file

--- Learning/JavaScript/class_1.js
+++ Learning/JavaScript/class_1.js
@@ -5,29 +5,27 @@ const password = null
 
 console.log(userName)
 console.log(password)
-// console.error("")
-// console.warn("hardik")
+console.error("")
+console.warn("hardik")
 
 
-// let 
-// const
+let 
+const
 
 // Primitive DataType (STACK)
 // we store primitive data type in stack kind of memory
-/*
-string
+String
 Number
 Bool
 BigInt
 Symbol
 Null
 Undefined
-*/
+
 
 // Reference or Non-Primitive Datatype (HEAP)
 // we store non-primitive data type in heap kind of memory
 
-/*
 Array
 Object
 Functions
@@ -35,17 +33,10 @@ Map
 Set
 Date
 RegExp 
-var a = "Hello World"
-console.log(typeof a)
-var b = new String("Hello World")
-console.log(b instanceof String)
-var num = 10;
-console.log(typeof num);
-*/
 
-// let a = []
 
 // Comparison Operator
-
-// == :- Checks the value 
-// === :- Checks the DataType
+== :- Checks the value 
+=== :- Checks the DataType
+!= : Not operator
+!== : Not operator with DataType
