c1fe08cc5f1f7ea0b3df5694f3fd2a1c1753578a
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2023-12-25 21:26:26 +0530

    Update class1

--- Learning/JavaScript/class_1.js
+++ Learning/JavaScript/class_1.js
@@ -1,42 +1,42 @@
-// Types of console.log
+// // Types of console.log
 
-let userName
-const password = null
+// let userName
+// const password = null
 
-console.log(userName)
-console.log(password)
-console.error("")
-console.warn("hardik")
-
-
-let 
-const
-
-// Primitive DataType (STACK)
-// we store primitive data type in stack kind of memory
-String
-Number
-Bool
-BigInt
-Symbol
-Null
-Undefined
+// console.log(userName)
+// console.log(password)
+// console.error("")
+// console.warn("hardik")
 
 
-// Reference or Non-Primitive Datatype (HEAP)
-// we store non-primitive data type in heap kind of memory
+// let 
+// const
 
-Array
-Object
-Functions
-Map
-Set
-Date
-RegExp 
+// // Primitive DataType (STACK)
+// // we store primitive data type in stack kind of memory
+// String
+// Number
+// Bool
+// BigInt
+// Symbol
+// Null
+// Undefined
 
 
-// Comparison Operator
-== :- Checks the value 
-=== :- Checks the DataType
-!= : Not operator
-!== : Not operator with DataType
+// // Reference or Non-Primitive Datatype (HEAP)
+// // we store non-primitive data type in heap kind of memory
+
+// Array
+// Object
+// Functions
+// Map
+// Set
+// Date
+// RegExp 
+
+
+// // Comparison Operator
+// == :- Checks the value 
+// === :- Checks the DataType
+// != : Not operator
+// !== : Not operator with DataType
