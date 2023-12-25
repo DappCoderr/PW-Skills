143e5d28b4d6a9d0675199fd15bc4fa9d6713dbe
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2023-12-25 21:25:24 +0530

    Add class3 about if else and switch statement

---
+++ Learning/JavaScript/class_3.js
@@ -0,0 +1,49 @@
+// Control Statement 
+
+// ---------------------------------if else----------------------------------
+// let score = 101;
+// let life = 3
+// let bullet = 1000 
+
+// if(score > 99){
+//     console.log("You get a life up")
+//     score -= 100;
+//     life++
+//     bullet = 2000
+//     console.log(`Bullets: ${bullet}`)
+// } else {
+//     console.log("You don't get it")
+// }
+    
+// console.log(`Welcome to Game. You score is ${score} and you life is ${life}, Bullets: ${bullet}`)
+
+
+// ---------------------------------Switch Statement---------------------------------
+
+
+// let getStarFromUser = "4"
+// let starInNumber = Number(getStarFromUser)
+
+// switch (starInNumber) {
+//     case 1:
+//         console.log("Poor")
+//         break;
+//     case 2:
+//         console.log("Above Poor")
+//         break;
+//     case 3:
+//         console.log("Average")
+//         break;
+//     case 4:
+//         console.log("Good")
+//         break;
+//     case 5:
+//         console.log("Best")
+//         break;
+//     default:
+//         console.log("Star don't match the equation")
+//         break;
+// }
+
+
+// ---------------------------------Class End---------------------------------
