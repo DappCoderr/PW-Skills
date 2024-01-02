cd9922bddfadca25e3832aaaf58e6d016c3ff195
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-01-02 22:11:05 +0530

    ADD: function class

---
+++ Learning/JavaScript/function.js
@@ -0,0 +1,19 @@
+// What is difference between Function Declaration and Expression?
+/*
+If you seen first key word as function then it's declaration otherwise it's expression.
+
+function a(){} -> This is function declaration
+
+const h = function process () {} -> This is function expression + This is named function expression 
+const h = function () {} -> This is function expression + This is anonymous function expression
+const h = () => {} -> This is function expression
+
+
+Note: JavaScript is a Compiled and Interpreted language.
+*/
+
+// Why in JS we consider function as first call function?
+/*      -> You can store a function
+        -> pass function as an argument
+        -> Return a function from a function
+        */
