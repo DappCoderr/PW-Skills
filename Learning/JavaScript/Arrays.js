9fa72c4388ef55c81a6cde4b48b7939d0bf1734f
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-01-12 16:56:13 +0530

    Update Array file - add forEach, map, reduce, filter, every, some, from

--- Learning/JavaScript/Arrays.js
+++ Learning/JavaScript/Arrays.js
@@ -25,14 +25,65 @@ Notes ----
 //   console.log(e);
 // });
 
-function customForEach(arr, cb) {
-  for (let i = 0; i < arr.length; i++) {
-    cb(arr[i], i);
-  }
-}
+// function customForEach(arr, cb) {
+//   for (let i = 0; i < arr.length; i++) {
+//     cb(arr[i], i);
+//   }
+// }
 
-console.log(
-  customForEach([2, 4, 5, 6], (e, i) => {
-    console.log(`Index ${i} and Element ${e}`);
-  })
-);
+// console.log(
+//   customForEach([2, 4, 5, 6], (e, i) => {
+//     console.log(`Index ${i} and Element ${e}`);
+//     return e * e;
+//   })
+// );
+
+// function factorial(num) {
+//   return Array.from({ length: num }, (_, i) => i + 1).reduce(
+//     (acc, element) => acc * element,
+//     1
+//   );
+// }
+
+// function factorial(num) {
+//   return Array.from({ length: num }, (_, i) => i + 1);
+// }
+
+// // [ 1, 2, 3, 4, 5 ]
+
+// console.log(factorial(5));
+
+// function factorial(num) {
+//   return Array.from(Array(num), (_, i) => i + 1);
+// }
+
+// // [ 1, 2, 3, 4, 5 ]
+
+// console.log(factorial(5));
+
+// const product = [
+//   { id: 1, name: "Iphone", price: 20, quantity: 1 },
+//   { id: 2, name: "laptop", price: 40, quantity: 2 },
+//   { id: 3, name: "Shoes", price: 10, quantity: 3 },
+//   { id: 4, name: "T-Shirt", price: 5, quantity: 5 },
+// ];
+
+// console.log(
+//   product.reduce((acc, element) => {
+//     console.log(element);
+//     return element;
+//   }, 0)
+// );
+
+// reducer -> takes two value accumulator and current value
+// current value takes iterate over array
+// while accumulator is the initial value of the array.
+
+// Array method
+// ForEach, map, filter, reduce, from, slice, splice, pop, push, unshift, shit, reverse, every, some
+
+// Two ways to define Array -
+// 1. {length: 2} -> array like object.
+// 2. Array(4)
+
+console.log(Array.from({ length: 5 }, (_) => console.log(_)));
