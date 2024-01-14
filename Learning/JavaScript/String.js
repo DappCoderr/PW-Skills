4be2a94893d37c571dff8f40605f6b7f3b8ae5b6
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-01-14 21:59:40 +0530

    Update JS String and Regex

---
+++ Learning/JavaScript/String.js
@@ -0,0 +1,11 @@
+// String and Regex in Js
+
+// using of eval
+
+// const result = eval("4+5");
+// console.log(result);
+
+// const result = "3" / 4;
+// console.log(result);
+
+// string template using single backticks
